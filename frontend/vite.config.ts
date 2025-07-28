f0b542142fc74ebd0fc09c851b420bd27c54fa05
am1t-kumar <thorragnarak@gmail.com>
2025-07-28 23:36:42 +0530

    add: minor changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Accepts any incoming connection (localhost or network)
+    //port: 3000, // Change to your desired port (avoid conflicts)
+    hmr: {
+      host: '0.0.0.0', // Match server.host for Hot Module Replacement
+    },
+    port: 3010, // change here
+  },
+});
