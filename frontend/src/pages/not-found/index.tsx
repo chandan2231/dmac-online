1421774c4a4f0dec097cd0bf17a89301ee401ff1
am1t-kumar <thorragnarak@gmail.com>
2025-08-07 12:11:43 +0530

    fix: UI bug

--- frontend/src/pages/not-found/index.tsx
+++ frontend/src/pages/not-found/index.tsx
@@ -4,50 +4,57 @@ import { ROUTES } from '../../features/auth/auth.interface';
 import { useSelector } from 'react-redux';
 import type { RootState } from '../../store';
 import PageNotFoundImage from '../../assets/404/404.jpg';
+import useTimer from '../../hooks/useTimer';
 
 const PageNotFound = () => {
   const navigate = useNavigate();
+  const isReady = useTimer(200); // 200ms delay
   const { isAuthenticated } = useSelector((state: RootState) => state.auth);
 
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        px: 2,
+    <div
+      style={{
+        display: !isReady ? 'none' : 'flex',
       }}
     >
-      {/* SVG Illustration */}
       <Box
-        component="img"
-        src={PageNotFoundImage}
-        alt="404 Not Found"
         sx={{
-          maxWidth: 400,
-          width: '100%',
-          mb: 4,
-          backgroundColor: 'transparent',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+          px: 2,
         }}
-        loading="lazy"
-      />
-
-      {/* Message */}
-      <Typography variant="h6" color="text.secondary">
-        Page Not Found
-      </Typography>
-
-      {/* Button to go back */}
-      <Button
-        variant="contained"
-        sx={{ mt: 4 }}
-        onClick={() => navigate(isAuthenticated ? ROUTES.HOME : ROUTES.LOGIN)}
       >
-        {isAuthenticated ? 'Go to Home' : 'Go to Login'}
-      </Button>
-    </Box>
+        {/* SVG Illustration */}
+        <Box
+          component="img"
+          src={PageNotFoundImage}
+          alt="404 Not Found"
+          sx={{
+            maxWidth: 400,
+            width: '100%',
+            mb: 4,
+            backgroundColor: 'transparent',
+          }}
+        />
+
+        {/* Message */}
+        <Typography variant="h6" color="text.secondary">
+          Page Not Found
+        </Typography>
+
+        {/* Button to go back */}
+        <Button
+          variant="contained"
+          sx={{ mt: 4 }}
+          onClick={() => navigate(isAuthenticated ? ROUTES.HOME : ROUTES.LOGIN)}
+        >
+          {isAuthenticated ? 'Go to Home' : 'Go to Login'}
+        </Button>
+      </Box>
+    </div>
   );
 };
 
