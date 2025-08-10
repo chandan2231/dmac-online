e1128f443f24a18bd7c8e3bda1b888d59930813d
am1t-kumar <thorragnarak@gmail.com>
2025-08-10 12:46:41 +0530

    add

--- api/controllers/language.js
+++ api/controllers/language.js
@@ -1,8 +1,7 @@
 import { db } from '../connect.js'
 
-
 export const getLanguageList = (req, res) => {
-  const que = 'SELECT * FROM dmac_webapp_language WHERE status=?'
+  const que = 'SELECT id, language FROM dmac_webapp_language WHERE status=?'
   db.query(que, [1], (err, data) => {
     if (err) return res.status(500).json(err)
     if (data.length >= 0) {
