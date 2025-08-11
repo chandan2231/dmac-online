2338fe6a54ec8e4838104867771ec2fc74b7575c
Chandan <chandan@Chandans-MacBook-Air.local>
2025-08-11 23:39:25 +0530

    questionar and pages

--- api/routes/questionar.js
+++ api/routes/questionar.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getQuestionWithFollowUps } from '../controllers/questionar.js';
+import { getQuestionWithFollowUps, getPageContent } from '../controllers/questionar.js'
+import { authenticateUser } from '../utils/middleware.js'
 
 const router = express.Router()
 
 router.get('/:sequenceNo', getQuestionWithFollowUps);
+router.get('/page/:pageKey', getPageContent);
 
 export default router
