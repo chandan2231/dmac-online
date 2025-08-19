f81ed7125cf6bb328d6a99f3c366d1635c441b2e
am1t-kumar <thorragnarak@gmail.com>
2025-08-19 23:14:05 +0530

    ref: code ref

--- frontend/src/providers/language-constant-provider/index.tsx
+++ frontend/src/providers/language-constant-provider/index.tsx
@@ -1,13 +1,6 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext } from 'react';
 import type { ILanguageConstants } from '../../i18n/language.interface';
-import type { RootState } from '../../store';
-import { useSelector } from 'react-redux';
-import { LOCAL_STORAGE_KEYS } from '../../utils/constants';
-import {
-  getLocalStorageItem,
-  setLocalStorageItem,
-} from '../../utils/functions';
-import LanguageService from '../../i18n/language.service';
+import { useLanguageConstants } from './hooks/useLanguageConstants';
 
 interface ILanguageContextProps {
   languageConstants: ILanguageConstants | Record<string, string>;
@@ -30,50 +23,7 @@ export const useLanguageConstantContext = () => {
 export const LanguageConstantProvider: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
-  const { user, isAuthenticated } = useSelector(
-    (state: RootState) => state.auth
-  );
-
-  const [languageConstants, setLanguageConstants] = useState<
-    ILanguageConstants | Record<string, string>
-  >({});
-
-  useEffect(() => {
-    const fetchAndStoreLanguage = async () => {
-      if (!isAuthenticated || !user?.languageCode) {
-        setLanguageConstants({});
-        return;
-      }
-
-      try {
-        const storedConstants = getLocalStorageItem(
-          LOCAL_STORAGE_KEYS.LANGUAGE_CONSTANTS
-        );
-        const parsedConstants = storedConstants
-          ? JSON.parse(storedConstants)
-          : null;
-
-        if (parsedConstants) {
-          setLanguageConstants(parsedConstants);
-          return;
-        }
-
-        const langCode = user.languageCode ?? 'en';
-        const { data } = await LanguageService.fetchLanguageContants(langCode);
-
-        setLanguageConstants(data);
-        setLocalStorageItem(
-          LOCAL_STORAGE_KEYS.LANGUAGE_CONSTANTS,
-          JSON.stringify(data)
-        );
-      } catch (error) {
-        console.error('Error fetching language constants:', error);
-        setLanguageConstants({});
-      }
-    };
-
-    fetchAndStoreLanguage();
-  }, [isAuthenticated, user]);
+  const { languageConstants } = useLanguageConstants();
 
   return (
     <LanguageConstantContext.Provider value={{ languageConstants }}>
