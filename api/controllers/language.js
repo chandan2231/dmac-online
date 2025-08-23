6c722326b62398d798607364daeff530c9c6022b
Chandan <chandan@Chandans-MacBook-Air.local>
2025-08-23 20:08:54 +0530

    ypdate language api

--- api/controllers/language.js
+++ api/controllers/language.js
@@ -9,3 +9,17 @@ export const getLanguageList = (req, res) => {
     }
   })
 }
+
+export const updateLanguage = (req, res) => {
+  const que = 'UPDATE dmac_webapp_users SET language=? WHERE id=?'
+  db.query(que, [req.body.language, req.body.id], (err, data) => {
+    if (err) {
+      return res.status(500).json(err)
+    } else {
+      let result = {}
+      result.status = 200
+      result.msg = 'Language Changed Successfully'
+      return res.json(result)
+    }
+  })
+}
--- api/routes/language.js
+++ api/routes/language.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import {getLanguageList} from '../controllers/language.js'
+import {getLanguageList, updateLanguage} from '../controllers/language.js'
+import { authenticateUser } from '../utils/middleware.js'
 
 const router = express.Router()
 
 router.get('/language-list', getLanguageList)
+router.post('/language-update', authenticateUser, updateLanguage)
 
 export default router
