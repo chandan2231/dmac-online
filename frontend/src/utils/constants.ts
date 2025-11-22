43159b6eb1aeb3492288132072c2c6aef0653e26
am1t-kumar <thorragnarak@gmail.com>
2025-11-22 16:42:57 +0530

    remove unused ADMIN_CREDENTIALS constant from constants.ts

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -10,11 +10,6 @@ export const LOCAL_STORAGE_KEYS = {
 export const DRAWER_WIDTH = 240;
 export const MINI_DRAWER_WIDTH = 70;
 
-export const ADMIN_CREDENTIALS = {
-  email: 'ADMIN_CREDENTIALS@gmail.com',
-  password: 'ADMIN_CREDENTIALS',
-};
-
 export const COUNTRIES_LIST: IOption[] = [
   { value: 'AF', label: 'Afghanistan' },
   { value: 'AL', label: 'Albania' },
