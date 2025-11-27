c92ec4dac7000a6acb40675976847fb612b4eff3
Chandan <chandan@Chandans-MacBook-Air.local>
2025-11-27 18:02:29 +0530

    create the consultation

--- api/routes/googleAuth.js
+++ api/routes/googleAuth.js
@@ -1,10 +1,19 @@
 import express from 'express'
-import { googleAuthUrl, googleCallbackUrl } from '../controllers/googleAuth.js'
+import { 
+    googleAuthUrl, 
+    googleCallbackUrl, 
+    bookConsultationWithGoogleCalender,
+    rescheduleConsultationWithGoogleCalendar,
+    cancelConsultationByConsultant
+} from '../controllers/googleAuth.js'
 import { authenticateUserGoogle } from '../utils/middleware.js'
 
 const router = express.Router()
 
 router.get('/url', authenticateUserGoogle, googleAuthUrl)
 router.get('/auth/callback', googleCallbackUrl)
+router.post('/book/consultation', bookConsultationWithGoogleCalender)
+router.post('/reschedule/consultation', rescheduleConsultationWithGoogleCalendar)
+router.post('/cancel/consultation', cancelConsultationByConsultant)
 
 export default router
