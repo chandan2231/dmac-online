b032b6d19e8e5ed80cebdc6ba9bbba8f46aec68d
am1t-kumar <thorragnarak@gmail.com>
2025-12-04 15:22:31 +0530

    feat: add reviews feature for experts and therapists with routing and sidebar integration

---
+++ frontend/src/features/expert/reviews/index.tsx
@@ -0,0 +1,5 @@
+const Reviews = () => {
+  return <div>Reviews Component</div>;
+};
+
+export default Reviews;
---
+++ frontend/src/pages/expert/reviews/index.tsx
@@ -0,0 +1,7 @@
+import Reviews from '../../../features/expert/reviews';
+
+const ExpertReviewsPage = () => {
+  return <Reviews />;
+};
+
+export default ExpertReviewsPage;
