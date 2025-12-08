2fcd4b6d9c03b96be71d977aa709c2f88b1ab8f9
Yash <yashpatel2055@gmail.com>
2025-12-09 03:20:17 +0530

    remove name from constant

--- api/templates/constant.js
+++ api/templates/constant.js
@@ -1,6 +1,6 @@
 const continuingReviewQuestions = {
   riskAssessment: {
-    title: `Risk Assessment yash`,
+    title: `Risk Assessment`,
     question1: {
       text: `Since the date of the last approval, has any regulatory agency
                 including, but not limited to, the sponsor, statistical agency,
