084b9b68fe99d77a8b4a190f092ea0ea8fb0f660
Yash <yashpatel2055@gmail.com>
2025-12-18 01:46:35 +0530

    remove language change button disbale code

--- frontend/src/features/patient/questioners/index.tsx
+++ frontend/src/features/patient/questioners/index.tsx
@@ -1,32 +1,23 @@
 import { Box } from '@mui/material';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Disclaimer from './components/Disclaimer';
 import FalsePositive from './components/FalsePositive';
 import Questions from './components/Questioners';
 
 import ModuleRunner from './components/GameModules/ModuleRunner';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import type { RootState } from '../../../store';
 import { get } from 'lodash';
-import { disableLanguageSelector, enableLanguageSelector } from '../../../i18n/language.slice';
+
 
 const Questioners = () => {
-  const dispatch = useDispatch();
+
   const [isQuestionerClosed, setIsQuestionerClosed] = useState(false);
   const [isDisclaimerAccepted, setIsDisclaimerAccepted] = useState(false);
   const [falsePositive, setFalsePositive] = useState(false);
   const { user } = useSelector((state: RootState) => state.auth);
 
-  // Disable language selector when game starts
-  useEffect(() => {
-    if (isQuestionerClosed) {
-      dispatch(disableLanguageSelector());
-    }
-  }, [isQuestionerClosed, dispatch]);
-
   const handleAllModulesComplete = () => {
-    // Re-enable language selector when game completes
-    dispatch(enableLanguageSelector());
     // Handle completion, maybe navigate home?
   };
 
